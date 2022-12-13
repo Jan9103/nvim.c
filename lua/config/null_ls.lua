8edd81a9bcec742c153c89c2dd2e4af8093585d0
Jan9103 <jan9103.wargear@protonmail.com>
2022-12-13 15:13:17 +0100

    fix duplicate vale linting

--- lua/config/null_ls.lua
+++ lua/config/null_ls.lua
@@ -3,6 +3,6 @@ local null_ls = require('null-ls')
 null_ls.setup({
 	sources = {
 		null_ls.builtins.code_actions.gitsigns,
-		null_ls.builtins.diagnostics.vale,
+		--null_ls.builtins.diagnostics.vale, -- moved to nvim-lint
 	},
 })
