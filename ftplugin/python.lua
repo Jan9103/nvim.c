9a2ecc3a2bf4ec4e683a20b39e957a14f24c58a8
shae <jan9103.wargear@protonmail.com>
2023-03-11 18:53:45 +0100

    Remove some legacy code

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,7 +1,2 @@
 -- luacheck: globals vim
 vim.g.python_recommended_style = 0 -- disable pep8 auto tab expand
-
--- vanilla syntax highlight
-vim.b.python_space_error_highlight = 1
-
-vim.b.ropevim_vim_completion = 1
