7c79163111d792be84df1415d21dc045dcfbda0a
shae <jan9103.wargear@protonmail.com>
2023-04-11 21:49:45 +0200

    Add autosort to csv

--- ftplugin/csv.lua
+++ ftplugin/csv.lua
@@ -1,10 +1,10 @@
 -- luacheck: globals vim
 
-local augroup = vim.api.nvim_create_augroup('TODOTXT_FT_AU', {})
+local augroup = vim.api.nvim_create_augroup('CSV_FT_AU', {})
 
 vim.api.nvim_create_autocmd("BufWritePre", {
 	callback = function()
-		vim.cmd("sor i")
+		vim.cmd("2,$sor i")
 	end,
 	group = augroup,
 })
