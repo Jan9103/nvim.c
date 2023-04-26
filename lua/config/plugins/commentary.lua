f96e75c2b5e7b12b93d013c236f02c0dae13aabf
shae <jan9103.wargear@protonmail.com>
2023-04-27 01:09:51 +0200

    feat: replace mini-comment with comment.lua
    comment.lua allow `([|])` + `)` -> `([])|`

--- lua/config/plugins/commentary.lua
+++ lua/config/plugins/commentary.lua
@@ -1,8 +1,40 @@
-return {
-	'echasnovski/mini.comment',
+local M = {
+	'numToStr/Comment.nvim',
 	keys = {
-		'gcc',
+		-- line comments
+		'gc',
 		{'gc', mode = 'v'},
+		-- block comments
+		'gb',
+		{'gb', mode = 'v'},
 	},
-	config = function() require('mini.comment').setup() end,
 }
+
+function M.config()
+	require('Comment').setup({
+		padding = true,
+		sticky = true,  -- keep cursor position
+		ignore = nil,
+		toggler = {
+			line = 'gcc',
+			block = 'gbc',
+		},
+		opleader = {
+			line = 'gc',
+			block = 'gb',
+		},
+		extra = {
+			above = 'gcO',
+			below = 'gco',
+			eol = 'gcA',
+		},
+		mappings = {
+			basic = true,
+			extra = true,
+		},
+		pre_hook = nil,  -- function to call before
+		post_hook = nil,  -- function to call after
+	})
+end
+
+return M
