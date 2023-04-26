cb64b02e3323cad29ecc1f240af193d17ba67c4b
shae <jan9103.wargear@protonmail.com>
2023-04-27 01:09:03 +0200

    refactor: use `<leader>` more

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- luacheck: globals vim
-vim.keymap.set('n', 'f', '')
-vim.keymap.set('n', 'F', '')
+--vim.keymap.set('n', 'f', '')
+--vim.keymap.set('n', 'F', '')
+vim.g.mapleader = 'f'
 
 require('config.lazy')  -- since lazy applies lua caching run it first
 require('config.design').setup()
