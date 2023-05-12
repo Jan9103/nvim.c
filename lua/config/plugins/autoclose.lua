9406b92190f9d42ba71b360b8a0476e71aa1d86c
shae <jan9103.wargear@protonmail.com>
2023-05-12 20:12:32 +0200

    fix(autoclose): disable custom config since it broke

--- lua/config/plugins/autoclose.lua
+++ lua/config/plugins/autoclose.lua
@@ -1,41 +1,43 @@
 local M = {
+	--enabled = false,
 	'altermo/ultimate-autopair.nvim',
+	branch = 'v0.4.0',
 	event = {'InsertEnter', 'CmdlineEnter'},
 }
 
 function M.config()
 	local ap = require('ultimate-autopair')
-	ap.setup({
-		mapopt = {noremap = true},
-		cmap = true,
-		bs = {
-			enable = true,
-			overjump = true,
-			space = true,
-			multichar = true,
-			fallback = nil,
-		},
-		cr = {
-			enable = true,
-			autoclose = false,
-			multichar = {
-				enable = true,
-				markdown = {{'```', '```', pair = true, noalpha = true, next = true}},
-			},
-			addsemi = {'c', 'cpp', 'rust'},
-			fallback = nil,
-		},
-		space = {
-			enable = true,
-			fallback = nil,
-		},
-		fastwarp = {
-			enable = false,
-		},
-		fastend = {
-			enable = false,
-		},
-	})
+	ap.setup()
+	-- ap.setup({
+	-- 	cmap = true,
+	-- 	bs = {
+	-- 		enable = true,
+	-- 		overjump = true,
+	-- 		space = true,
+	-- 		multichar = true,
+	-- 		fallback = nil,
+	-- 	},
+	-- 	cr = {
+	-- 		enable = true,
+	-- 		autoclose = false,
+	-- 		multichar = {
+	-- 			enable = true,
+	-- 			markdown = {{'```', '```', pair = true, noalpha = true, next = true}},
+	-- 		},
+	-- 		addsemi = {'c', 'cpp', 'rust'},
+	-- 		fallback = nil,
+	-- 	},
+	-- 	space = {
+	-- 		enable = true,
+	-- 		fallback = nil,
+	-- 	},
+	-- 	fastwarp = {
+	-- 		enable = false,
+	-- 	},
+	-- 	--fastend = {
+	-- 	--	enable = false,
+	-- 	--},
+	-- })
 end
 
 return M
